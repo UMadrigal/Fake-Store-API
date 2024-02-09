d121411d4e823da2adf1799bd8497f5c15896fd2
UMadrigal <umadrigalc@gmail.com>
2024-02-09 11:01:01 -0600

    Login UI v3

--- app/src/main/java/com/macruware/fakestore/ui/LoginActivity.kt
+++ app/src/main/java/com/macruware/fakestore/ui/LoginActivity.kt
@@ -2,11 +2,68 @@ package com.macruware.fakestore.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.macruware.fakestore.R
+import com.macruware.fakestore.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    private var isShowPassword = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListeners()
+        initUI()
+    }
+
+    private fun initListeners() {
+        binding.btnShowPassword.setOnClickListener {
+            isShowPassword = !isShowPassword
+            if (isShowPassword) {
+                binding.btnShowPassword.setImageResource(R.drawable.ic_show_password_true)
+                binding.etPassword.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+            } else {
+                binding.btnShowPassword.setImageResource(R.drawable.ic_show_password_false)
+                binding.etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+            // Mover el cursor al final del texto
+            binding.etPassword.setSelection(binding.etPassword.text?.length ?: 0)
+        }
+
+        binding.btnGoToRegister.setOnClickListener {
+//            findNavControllar navigate
+            Toast.makeText(this,"Ir a registro",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.etPassword.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                (event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)
+            ) {
+                // Ocultar teclado
+                val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(binding.etPassword.windowToken, 0)
+
+                // Ocultar el cursor y quitar el foco del EditText
+                binding.etPassword.clearFocus()
+
+                return@setOnEditorActionListener true
+            }
+            false
+        }
+    }
+
+    private fun initUI() {
+
     }
 }
